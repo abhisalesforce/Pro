7599f8411ef51b3170a40536a3612b9a022e97e1
abhishek mutyampeta <abhi.abhishek594@gmail.com>
2017-11-20 12:00:34 -0600

    clause

---
+++ Clause.cpp
@@ -0,0 +1,11 @@
+#include "Clause.h"
+
+using namespace std;
+
+Clause::Clause(int len, vector<int> &hgs, int base, int neck, vector<int> &xs) {
+    this->hgs = hgs;
+    this->base = base;
+    this->len = len;
+    this->neck = neck;
+    this->xs = xs;
+}
